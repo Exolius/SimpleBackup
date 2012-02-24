ec6f59a8d4b436e447cc07229a663d49670a97a3
Jamaal Primus <j.primus800@gmail.com>
2012-02-24 07:52:18 +0000

    Testing Intellij GitHub integration.

--- src/com/exolius/simplebackup/SimpleBackup.java
+++ src/com/exolius/simplebackup/SimpleBackup.java
@@ -14,7 +14,6 @@ import java.util.Date;
 import java.util.List;
 
 public class SimpleBackup extends JavaPlugin {
-     //Testing Intellij Github Integration.
      /*----------------------
      Variable declerations
      ----------------------*/
