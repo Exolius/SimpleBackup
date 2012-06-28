d220bc0897d650791e9f0c48b8f733ad5e687055
Jamaal Primus <j.primus800@gmail.com>
2012-06-28 08:15:36 +0100

    Updated plugin source/readme files from Mantuns fork. https://github.com/mantun/SimpleBackup
    Fixed error with code that caused zipping backups to mor execute.
    
    Updated to Bukkit #02222 Reccomended build

--- src/com/exolius/simplebackup/PluginUtils.java
+++ src/com/exolius/simplebackup/PluginUtils.java
@@ -1,29 +1,152 @@
 package com.exolius.simplebackup;
 
 import java.io.File;
-
-import static java.lang.System.currentTimeMillis;
-import static java.lang.System.out;
+import java.text.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PluginUtils {
 
-    //not implemented yet.
-    public static void deleteOldBackups(int daysBack, String dirWay) {
-        File directory = new File(dirWay);
-        if (directory.exists()) {
-            File[] listFiles = directory.listFiles();
-            long purgeTime = currentTimeMillis() - (daysBack * 24L * 60L * 60L * 1000L);
-            for (File listFile : listFiles) {
-                if (listFile.lastModified() > purgeTime) {
-                    out.println("[Simple Backup] Deleting oldest file:" + listFile);
-                    if (listFile.isFile()) {
-                        listFile.delete();
-                    } else {
-                        deleteOldBackups(daysBack, listFile.getAbsolutePath());
-                    }
+    public static class DateModification {
+        private int field;
+        private int amount;
+
+        public DateModification(int field, int amount) {
+            this.field = field;
+            this.amount = amount;
+        }
+
+        public void moveForward(Calendar cal) {
+            if (amount == 0) {
+                throw new UnsupportedOperationException();
+            }
+            cal.add(field, amount);
+        }
+
+        public void moveBack(Calendar cal) {
+            if (amount == 0) {
+                throw new UnsupportedOperationException();
+            }
+            cal.add(field, -amount);
+        }
+
+        public static DateModification fromString(String s) {
+            if ("0".equals(s)) {
+                return new DateModification(Calendar.DATE, 0);
+            }
+            Matcher matcher = Pattern.compile("(\\d+)([hdwmyHDWMY])").matcher(s);
+            if (matcher.matches()) {
+                String countStr = matcher.group(1);
+                String unitStr = matcher.group(2);
+                int count;
+                try {
+                    count = Integer.parseInt(countStr);
+                } catch (NumberFormatException e) {
+                    return null;
                 }
+                int unit;
+                if ("h".equalsIgnoreCase(unitStr)) {
+                    unit = Calendar.HOUR;
+                } else if ("d".equalsIgnoreCase(unitStr)) {
+                    unit = Calendar.DATE;
+                } else if ("w".equalsIgnoreCase(unitStr)) {
+                    unit = Calendar.WEEK_OF_YEAR;
+                } else if ("m".equalsIgnoreCase(unitStr)) {
+                    unit = Calendar.MONTH;
+                } else if ("y".equalsIgnoreCase(unitStr)) {
+                    unit = Calendar.YEAR;
+                } else {
+                    return null;
+                }
+                return new DateModification(unit, count);
+                
+            }
+            return null;
+        }
+    }
+    
+    public static void deleteOldBackups(File backupFile, String dateFormat, List<DateModification> intervals, List<DateModification> frequencies) {
+        if (intervals.isEmpty() || frequencies.isEmpty()) {
+            return;
+        }
+        // collect files
+        SortedMap<Date, File> oldFiles = collectFiles(backupFile, dateFormat);
+        if (oldFiles.isEmpty()) {
+            return;
+        }
+
+        // delete unwanted files in each interval
+        Calendar intervalEnd = Calendar.getInstance();
+        intervalEnd.setTime(oldFiles.lastKey());
+        intervals.get(0).moveBack(intervalEnd);
+        for (int i = 1; i <= intervals.size(); i++) {
+            Calendar intervalStart = null;
+            if (i < intervals.size()) {
+                intervalStart = (Calendar) intervalEnd.clone();
+                intervals.get(i).moveBack(intervalStart);
+            }
+            if (i <= frequencies.size() && frequencies.get(i - 1) != null) {
+                deleteExtraBackups(filter(oldFiles, intervalStart, intervalEnd), frequencies.get(i - 1));
+            }
+            intervalEnd = intervalStart;
+        }
+
+    }
+
+    private static SortedMap<Date, File> collectFiles(File backupFile, String dateFormat) {
+        File[] files = backupFile.listFiles();
+        if (files == null) {
+            return null;
+        }
+        SortedMap<Date, File> oldFiles = new TreeMap<Date, File>();
+        SimpleDateFormat formatter = new SimpleDateFormat(dateFormat);
+        for (File file : files) {
+            Date date = formatter.parse(file.getName(), new ParsePosition(0));
+            if (date != null) {
+                oldFiles.put(date, file);
             }
         }
+        return oldFiles;
+    }
+
+    private static SortedMap<Date, File> filter(SortedMap<Date, File> oldFiles, Calendar from, Calendar to) {
+        SortedMap<Date, File> result = new TreeMap<Date, File>(oldFiles);
+        Iterator<Date> it = result.keySet().iterator();
+        while (it.hasNext()) {
+            Date date = it.next();
+            if (from != null && date.before(from.getTime()) || !date.before(to.getTime())) {
+                it.remove();
+            }
+        }
+        return result;
+    }
+
+    private static void deleteExtraBackups(SortedMap<Date, File> files, DateModification desiredFrequency) {
+        if (files.isEmpty()) {
+            return;
+        }
+        Calendar nextDate = null;
+        for (Date date : files.keySet()) {
+            if (desiredFrequency.amount == 0) {
+                deleteBackup(files.get(date));
+            } else if (nextDate == null) {
+                nextDate = Calendar.getInstance();
+                nextDate.setTime(date);
+                desiredFrequency.moveForward(nextDate);
+            } else if (!date.before(nextDate.getTime())) {
+                do {
+                    desiredFrequency.moveForward(nextDate);
+                } while (!date.before(nextDate.getTime()));
+            } else {
+                deleteBackup(files.get(date));
+            }
+        }
+    }
+
+    private static void deleteBackup(File file) {
+        System.out.println("[SimpleBackup] Deleting backup " + file.getPath());
+        file.delete();
     }
 
 }
--- src/com/exolius/simplebackup/SimpleBackup.java
+++ src/com/exolius/simplebackup/SimpleBackup.java
@@ -1,46 +1,43 @@
 package com.exolius.simplebackup;
 
+import com.exolius.simplebackup.PluginUtils.DateModification;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
+import java.io.*;
+import java.net.URI;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class SimpleBackup extends JavaPlugin {
-    /*----------------------
-    Variable declerations
-    ----------------------*/
     double interval;
 
     public boolean broadcast = true;
-    public boolean firstRunDelay = true;
     public boolean disableZipping = false;
-    public boolean deleteOldMaps = false;
+    public boolean selfPromotion = false;
 
-    public static String message = "[SimpleBackup]";
-    public static String dateFormat = "yyyy-MM-dd-HH-mm-ss";
-    public static String backupFile = "backups/";
+    public String message = "[SimpleBackup]";
+    public String dateFormat = "yyyy-MM-dd-HH-mm-ss";
+    public String backupFile = "backups/";
     public String customMessage = "Backup starting";
-    public static String pluginLocation;
 
     List<String> backupWorlds;
-
-    public static int intervalBetween = 100;
+    List<DateModification> deleteScheduleIntervals;
+    List<DateModification> deleteScheduleFrequencies;
 
     protected FileConfiguration config;
 
-    public int daysToDelete = 2;
 
     /*----------------------------------------
      This is ran when the plugin is disabled
      -----------------------------------------*/
+    @Override
     public void onDisable() {
         getServer().getScheduler().cancelTasks(this);
         System.out.println("[SimpleBackup] Disabled SimpleBackup");
@@ -49,12 +46,12 @@ public class SimpleBackup extends JavaPlugin {
     /*----------------------------------------
      This is ran when the plugin is enabled
      -----------------------------------------*/
+    @Override
     public void onEnable() {
         // When plugin is enabled, load the "config.yml"
         loadConfiguration();
-        pluginLocation = "" + this.getDataFolder();
         // Set the backup interval, 72000.0D is 1 hour, multiplying it by the value interval will change the backup cycle time
-        double ticks = 72000.0D * this.interval;
+        long ticks = (long) (72000 * this.interval);
 
         // Add the repeating task, set it to repeat the specified time
         this.getServer().getScheduler().scheduleAsyncRepeatingTask(this, new Runnable() {
@@ -63,30 +60,22 @@ public class SimpleBackup extends JavaPlugin {
                 // When the task is run, start the map backup
                 doBackup();
             }
-        }, 60L, (long) ticks);
+        }, ticks, ticks);
 
         // After enabling, print to console to say if it was successful
         System.out.println("[SimpleBackup] Enabled. Backup interval " + this.interval + " hours");
         // Shameless self promotion in the source code :D
-        System.out.println("[SimpleBackup] Developed by Exolius");
+        if(selfPromotion){System.out.println("[SimpleBackup] Developed by Exolius");}
     }
 
     /*-------------------------------------------------------
      This is ran when the plugin command is sent by a player
      --------------------------------------------------------*/
+    @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        Player player = null;
-        if (sender instanceof Player) {
-            player = (Player) sender;
-        }
-
         if (cmd.getName().equalsIgnoreCase("sbackup")) {
-            if (player == null) {
-                sender.sendMessage("this command can only be run by a player");
-            } else {
-                if (player.isOp()) {
-                    doBackup();
-                }
+            if (sender.isOp()) {
+                doBackup();
             }
             return true;
         }
@@ -101,141 +90,134 @@ public class SimpleBackup extends JavaPlugin {
         config = getConfig();
 
         // Set default values for variables
-        interval = config.getDouble("backup-interval-hours", 1.0D);
-        intervalBetween = config.getInt("interval-between", intervalBetween);
-        broadcast = config.getBoolean("broadcast-message", true);
-        backupFile = config.getString("backup-file", backupFile);
+        interval = config.getDouble("backup-interval-hours");
+        broadcast = config.getBoolean("broadcast-message");
+        backupFile = config.getString("backup-file");
         backupWorlds = config.getStringList("backup-worlds");
-        message = config.getString("backup-message", message);
-        dateFormat = config.getString("backup-date-format", dateFormat);
-        firstRunDelay = config.getBoolean("delay-first-backup", firstRunDelay);
-        customMessage = config.getString("custom-backup-mesasge", customMessage);
-        disableZipping = config.getBoolean("disable-zipping", disableZipping);
-
-        //These haven't been implemented yet
-        //deleteOldMaps = config.getBoolean("delete-old-maps", deleteOldMaps);
-        //daysToDelete = config.getInt("days-to-delete", daysToDelete);
-
-        if (backupWorlds.size() == 0) {
-            // If "backupWorlds" is empty then fill it with the worlds
-            backupWorlds.add(((World) getServer().getWorlds().get(0)).getName());
+        message = config.getString("backup-message");
+        dateFormat = config.getString("backup-date-format");
+        customMessage = config.getString("custom-backup-message");
+        disableZipping = config.getBoolean("disable-zipping");
+        selfPromotion = config.getBoolean("self-promotion");
+        List<String> intervalsStr = config.getStringList("delete-schedule.intervals");
+        List<String> frequenciesStr = config.getStringList("delete-schedule.interval-frequencies");
+        List<DateModification> intervals = new ArrayList<DateModification>();
+        List<DateModification> frequencies = new ArrayList<DateModification>();
+        for (int i = 0; i < intervalsStr.size(); i++) {
+            String is = intervalsStr.get(i);
+            DateModification interval = DateModification.fromString(is);
+            if (interval == null) {
+                System.out.println("[SimpleBackup] Can't parse interval " + is);
+                if (i < frequenciesStr.size()) {
+                    frequenciesStr.remove(i);
+                }
+            } else {
+                intervals.add(interval);
+            }
         }
+        for (String fs : frequenciesStr) {
+            DateModification f = DateModification.fromString(fs);
+            if (f == null) {
+                System.out.println("[SimpleBackup] Can't parse frequency " + fs);
+            }
+            frequencies.add(f);
+        }
+        deleteScheduleIntervals = intervals;
+        deleteScheduleFrequencies = frequencies;
+        
 
-        // Generate the default config.yml
-        config.set("backup-worlds", backupWorlds);
-        config.set("interval-between", Integer.valueOf(intervalBetween));
-        config.set("backup-interval-hours", Double.valueOf(interval));
-        config.set("broadcast-message", Boolean.valueOf(broadcast));
-        config.set("backup-file", backupFile);
-        config.set("backup-message", message);
-        config.set("backup-date-format", dateFormat);
-        config.set("delay-first-backup", firstRunDelay);
-        config.set("custom-backup-message", customMessage);
-        config.set("disable-zipping", disableZipping);
-
-        //These haven't been implemented yet
-        //config.set("delete-old-maps", deleteOldMaps);
-        //config.set("days-to-delete", daysToDelete);
 
         //Save the configuration file
+        config.options().copyDefaults(true);
         saveConfig();
+
+        if (backupWorlds.isEmpty()) {
+            // If "backupWorlds" is empty then fill it with the worlds
+            for (World world : getServer().getWorlds()) {
+                backupWorlds.add(world.getName());
+            }
+        }
     }
 
     /*------------------------
      This runs the map backup
      -------------------------*/
     public void doBackup() {
-        if (firstRunDelay) {
-            firstRunDelay = false;
-        } else {
-            // Begin backup of worlds
-            // Broadcast the backup initialization if enabled
-            SimpleBackup.this.getServer().broadcastMessage(ChatColor.BLUE + message + " " + customMessage);
-            // Loop through all the specified worlds and save them
-            for (World world : SimpleBackup.this.getServer().getWorlds()) {
+        // Begin backup of worlds
+        // Broadcast the backup initialization if enabled
+        if (broadcast) {
+            getServer().broadcastMessage(ChatColor.BLUE + message + " " + customMessage);
+        }
+        // Loop through all the specified worlds and save them
+        for (World world : getServer().getWorlds()) {
+            if (backupWorlds.contains(world.getName())) {
+                File targetFolder = new File(backupFile, world.getName());
+                world.setAutoSave(false);
                 try {
+                    world.save();
+                    System.out.println("[SimpleBackup] Backing up " + world.getWorldFolder());
                     if (disableZipping) {
-                        //If zipping is disabled then just copy the world folders to the backup directory
-                        if ((this.backupWorlds.contains(world.getName()))) {
-                            world.save();
-                            world.setAutoSave(false);
-                            System.out.println("[SimpleBackup] Backing up " + world.getWorldFolder());
-                            FileUtils.copyFiles(world.getWorldFolder(), new File(SimpleBackup.backupFile + "/" + world.getName() + "/" + SimpleBackup.format()));
-                            world.setAutoSave(true);
-                        }
+                        FileUtils.copyFiles(world.getWorldFolder(), new File(targetFolder, formatFileName()));
                     } else {
-                        //If zipping isnt disabled proceed to zip up the world folders then copy them
-                        BackupThread bt = SimpleBackup.this.backupWorld(world, null);
-                        if (bt != null) {
-                            world.save();
-                            world.setAutoSave(false);
-                            bt.start();
-                            while (bt.isAlive()) ;
-                            world.setAutoSave(true);
-                        }
-
+                        zipFiles(world.getWorldFolder(), new File(targetFolder, formatFileName() + ".zip"));
                     }
-                } catch (Exception e) {
+                } catch (IOException e) {
                     e.printStackTrace();
+                } finally {
+                    world.setAutoSave(true);
                 }
+                PluginUtils.deleteOldBackups(targetFolder, dateFormat, deleteScheduleIntervals, deleteScheduleFrequencies);
             }
+        }
 
-            // Broadcast the backup completion if enabled
-            if (SimpleBackup.this.broadcast)
-                SimpleBackup.this.getServer().broadcastMessage(ChatColor.BLUE + message + " Backup completed.");
-
-            // Old map file deletion goes here
-
+        // Broadcast the backup completion if enabled
+        if (broadcast) {
+            getServer().broadcastMessage(ChatColor.BLUE + message + " Backup completed.");
         }
     }
 
-    public BackupThread backupWorldNoZip(World world, File file)
-            throws Exception {
-        if (world != null) {
-            if ((this.backupWorlds.contains(world.getName()))) {
-                return new BackupThread(new File(world.getName()));
+    private void zipFiles(File sourceFolder, File destinationFile) throws IOException {
+        if (!destinationFile.getParentFile().exists()) {
+            destinationFile.getParentFile().mkdirs();
+        }
+        ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(destinationFile));
+        try {
+            zipFiles(sourceFolder.toURI(), sourceFolder, zip);
+        } finally {
+            try {
+                zip.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            return null;
         }
-        if ((world == null) && (file != null)) {
-            return new BackupThread(file);
-        }
-        return null;
     }
 
-    public BackupThread backupWorld(World world, File file)
-            throws Exception {
-        if (world != null) {
-            if ((this.backupWorlds.contains(world.getName()))) {
-                return new BackupThread(new File(world.getName()));
+    private void zipFiles(URI root, File source, ZipOutputStream zip) throws IOException {
+        if (source.isDirectory()) {
+            for (String file : source.list()) {
+                zipFiles(root, new File(source, file), zip);
             }
-            return null;
+        } else {
+            ZipEntry entry = new ZipEntry(root.relativize(source.toURI()).getPath());
+            zip.putNextEntry(entry);
+            InputStream in = new FileInputStream(source);
+            try {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = in.read(buffer)) > 0) {
+                    zip.write(buffer, 0, bytesRead);
+                }
+            } finally {
+                in.close();
+            }
         }
-        if ((world == null) && (file != null)) {
-            return new BackupThread(file);
-        }
-        return null;
     }
 
-    public static String format() {
+    public String formatFileName() {
         //Set the naming format of the backed up file, based on the config values
         Date date = new Date();
         SimpleDateFormat formatter = new SimpleDateFormat(dateFormat);
         return formatter.format(date);
-    }
-
-    //Prints debugging information, only used for development and is never ran
-    //with a plugin release.
-    public void printDebug() {
-        // Debugging code, prints loaded variables to console
-        // To see if the loading works after modification
-        System.out.println(backupWorlds);
-        System.out.println(intervalBetween);
-        System.out.println(interval);
-        System.out.println(broadcast);
-        System.out.println(backupFile);
-        System.out.println(message);
-        System.out.println(dateFormat);
     }
 
 }
