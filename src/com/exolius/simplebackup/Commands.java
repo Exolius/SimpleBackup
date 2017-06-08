c1ac079f9578c010dc3dd67d98a8a3d035a22629
RobinMC <robinslot@hotmail.nl>
2017-06-08 20:50:12 +0200

    Remove unused import

--- src/com/exolius/simplebackup/Commands.java
+++ src/com/exolius/simplebackup/Commands.java
@@ -1,6 +1,5 @@
 package com.exolius.simplebackup;
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
